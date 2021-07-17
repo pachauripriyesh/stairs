932f14ae404104a3bc23c900836fe9da93456d97
pachauripriyesh <87222591+pachauripriyesh@users.noreply.github.com>
2021-07-17 08:03:29 -0700

    Add files via upload

---
+++ stair.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("01\n");
+	printf("0101\n");
+	printf("010101\n");
+	printf("01010101\n");
+	printf("0101010101\n");
+	printf("010101010101\n");
+	return 0;
+}
